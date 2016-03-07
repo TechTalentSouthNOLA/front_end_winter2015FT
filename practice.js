87497f80f949cd4752448226499700ef0f7d7384
Sia <siakaramalegos@gmail.com>
2016-03-07 11:07:58 -0600

    Finished beginner JS

--- practice.js
+++ practice.js
@@ -70,4 +70,130 @@
 
 
 // ********** Scope *************************
+// This variable is in the Global scope
+// var x = "I'm a global variable called x";
+// // console.log(x);
 
+// // Define a function called someFunction
+// function someFunction(){
+//   // This variable only exists inside someFunction
+//   var y = "I'm a local variable called y";
+//   // console.log(x);
+//   // console.log(y);
+
+//   // Z is automatically global because I forgot my "var"
+//   z = 10;
+//   console.log(z);
+// }
+
+// someFunction();
+// // console.log(x);
+// // console.log(y); // y is not defined
+// console.log(z); // gives 10 because we made it auto global
+
+
+
+
+// *********** Operators *************
+// var x = 10,
+//     y = 5;
+
+// console.log(x + y);  // 15
+// console.log(x - y);  // 5
+// console.log(x * y);  // 50
+// console.log(x / 8);  // Regular division 1.25
+// console.log(Math.floor(x / 8)); // Integer division 1
+// console.log(x % 8);  // Modulus 2
+// console.log(Math.pow(x, y));  // 100000
+
+// console.log(x > y);  // true
+// console.log(x < y);  // false
+// console.log(x >= y); // true
+// console.log(x <= y); // false
+
+// console.log(x === y); // false
+// console.log(x !== y); // true
+
+// var a = true,
+//     b = false;
+
+// console.log(a && b);
+// console.log(a || b);
+
+
+
+
+// *********** Conditionals ***********
+// var x = 10,
+//     y = 10;
+
+// if (x > y){
+//   console.log("x is greater than y");
+// } else if (x < y){
+//   console.log("x is less than y");
+// } else {
+//   console.log("x is neither less than or greater than y");
+// }
+
+
+
+
+// *********** Loops ***************
+
+// C-style loop
+// var myArray = [1, 2, 3, 4, 5];
+
+// in parentheses, first declare i and set to zero, then tell it when to stop, then tell it what to increment i by
+// i++ is the same as i += 1 is the same as i = i + 1
+// for (var i = 0; i < myArray.length; i++){
+//   console.log(myArray[i]);
+// }
+
+// For each loop
+// myArray.forEach(function (element){
+//   console.log(element);
+// });
+
+// While loop
+// var i = 0;
+
+// while (i < myArray.length){
+//   console.log(myArray[i]);
+//   i++;
+// }
+
+
+
+
+// ********** Functions *************
+
+// A function with explicit arguments
+// function sumExplicitly(a, b){
+//   console.log(a + b);
+// }
+
+// sumExplicitly(18, 2);
+
+// // A function with implicit arguments
+// function sumImplicitly(){
+//   var total = 0,
+//       i;
+
+//   for (i = 0; i < arguments.length; i++){
+//     total += arguments[i];
+//   }
+
+//   console.log(total);
+// }
+
+// sumImplicitly(1, 2, 3, 4, 582);
+
+function sum(a,b){
+  return a + b;
+}
+
+var x = sum(10,5);
+var y = sum(x, sum(20, 30));
+console.log(x);
+console.log(y);
+console.log(sum(x,y));
