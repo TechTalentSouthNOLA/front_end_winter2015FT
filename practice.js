649ab167eca460ef508a6f6695b9435fa9c310ab
Sia <siakaramalegos@gmail.com>
2016-03-07 08:59:07 -0600

    beginner javascript

---
+++ practice.js
@@ -0,0 +1,73 @@
+// var message = "Hello, world!";
+// console.log(message);
+
+// var x = "hey";
+// var y = 10;
+
+// console.log(x);
+// console.log(y);
+// console.log(x + y);
+
+// y = " is for horses!";
+// console.log(x + y);
+
+// var a = 1, b = 2, c = "3";
+// console.log(a + b + c);
+
+
+
+
+// ******** Data types *********
+// Numbers
+// var integer_num = 1;
+// // var float_num = 1.23;
+// // console.log(integer_num + float_num);
+
+// // Strings
+// var message = "Let's learn JavaScript, ";
+// var name = "class";
+// console.log(message + name);
+
+// // Booleans
+// var is_cool = true;
+// console.log(is_cool + " dat");
+
+// // Arrays
+// var my_stuff = [integer_num, message, is_cool];
+// console.log(my_stuff);
+
+// Hashes
+// var capitals = {
+//   "LA": "Baton Rouge",
+//   "TX": "Austin",
+//   "GA": "Atlanta"
+// };
+// console.log(capitals["LA"]);
+
+
+
+
+// ********** Null and Undefined ***********
+// var x;
+
+// Variables that have NOT been initialized are UNDEFINED
+// console.log(x);
+
+// But they are NOT null
+// console.log(x === null);   // false
+
+// Unless you actually make them null
+// x = null;
+// console.log(x);                // null
+// console.log(x === null);       // true
+// console.log(x === undefined);  // false
+
+// Or if I accidentally used type coercion (only 2 equal signs instead of 3, it will mislead me.
+// console.log(x == undefined);   // true
+// console.log(x);                // null
+
+
+
+
+// ********** Scope *************************
+
